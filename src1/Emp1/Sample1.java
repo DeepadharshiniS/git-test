6e53b92d210db76fe40e88a5dfb4bebac394dc18
DeepadharshiniS <deepadharshini300400@gmail.com>
2021-06-02 15:51:17 +0530

    Employee2 changes

---
+++ src1/Emp1/Sample1.java
@@ -0,0 +1,14 @@
+package Emp1;
+
+public class Sample1 {
+
+	public void add() {
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
